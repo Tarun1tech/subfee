9d4061605de3cc30122f27958bcb0acf10d80630
latika gandhi <latika.gandhi@techstriker.com>
2022-06-28 18:39:34 +0530

    login redux

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -7,21 +7,25 @@ import { toast } from "react-toastify";
 export const do_login = payload => async dispatch => {
     let response = await doLogin(payload);
     response = checkResponse(response);
-  console.log(response,"response")
-if(response.success===true){
-    toast.success(response.message)
-    dispatch({
-        type: "LOGIN_SUCCESSFUL",
-        payload: response.data
-    });
-    localStorage.setItem("access_token", response.data.data.access_token)
-}else {
-    toast.error(response.error)
-    dispatch({
-        type: "LOGIN_FAILED",
-        payload: response.error
-    });
-    localStorage.clear();
-}    
+    console.log(response, "response")
+    if (response.success === true) {
+        toast.success(response.message)
+        dispatch({
+            type: "LOGIN_SUCCESSFUL",
+            payload: response.data
+        });
+        localStorage.setItem("access_token", response.data.data.access_token);
+        localStorage.setItem("success", response.data.data.success)
+    } else {
+        toast.error(response.error)
+        dispatch({
+            type: "LOGIN_FAILED",
+            payload: response.error
+        });
+        console.log(response.success)
+        localStorage.clear();
+
+        localStorage.setItem("success", response.success)
+    }
 };
 
