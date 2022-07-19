49afbf59c0f056e7e80b17122e5b09782785a934
Latika Gandhi <latika.gandhi@techstriker.com>
2022-07-19 18:43:16 +0530

    video comment issue fix

--- src/pages/dashboard/content/content.js
+++ src/pages/dashboard/content/content.js
@@ -118,13 +118,14 @@ const ContentPage = (props) => {
     description: "",
   }
 
-  const [commentValue, setcommentValue] = useState(true);
+  const [commentValue, setcommentValue] = useState(false);
 
   const handlecheckbox = (e) => {
     if ((e.target.checked)) {
       setcommentValue(!commentValue);
     }
   }
+  console.log(commentValue,"commentValue")
   const [formErrors, setFormErrors] = useState({});
   const [contentInputs, setcontentInputs] = useState(contentDetail);
   const validate = (values) => {
@@ -291,7 +292,7 @@ const ContentPage = (props) => {
                                 <p>Wil je toestaan dat subscribers onder deze video een comment kunnen plaatsen?</p>
                                 <div className="custom-comment-switch">
                                   <label className="switch">
-                                    <input type="checkbox" name="videoComments" onChange={handlecheckbox} />
+                                    <input type="checkbox" name="videoComments" onClick={handlecheckbox} />
                                     <span className="slider round"></span>
                                     <span className="ja">ja</span>
                                     <span className="nee">Nee</span>
@@ -327,7 +328,7 @@ const ContentPage = (props) => {
                               </svg> Thumbnail
                             </p>
                             <p className="mb-1">
-                              {commentValue === 1 ?
+                              {commentValue ?
                                 <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                   <path d="M4.409 9.8895L1.22701 6.7075L2.642 5.2925L4.409 7.0645L9.358 2.1105L10.773 3.5255L4.409 9.8895Z" fill="#65006B" />
                                 </svg> :
